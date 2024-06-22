1bcecaf717adaef51cddcdec7bac0f92da1d2e13
coderifki <rifkiramadhan.dev@gmail.com>
2024-06-22 21:23:51 +0700

    Feat: print transaction or export pdf

--- src/pages/transaction-management/transaction/table/transaction.table.tsx
+++ src/pages/transaction-management/transaction/table/transaction.table.tsx
@@ -1,31 +1,25 @@
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from '@tanstack/react-table'
+import React, { useEffect, useState, useRef } from 'react'
+import { jsPDF } from 'jspdf'
+import html2canvas from 'html2canvas'
 import {
   Button,
   Group,
   Loader,
   Pagination,
   Table as MantineTable,
-  Modal,
   createStyles,
   rem,
-  Grid,
-  Flex,
 } from '@mantine/core'
-
-import React, { useEffect, useState } from 'react'
+import {
+  useReactTable,
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+} from '@tanstack/react-table'
 import Link from 'next/link'
-import { formatNumberIndoCurrency } from '@/utils/formatNumberIndoCurrency'
-import { useDisclosure, useMediaQuery } from '@mantine/hooks'
-import { PaymentMethodEntity } from '@/features/transaction-management/payment/payment.model'
 import { format } from 'date-fns'
-import { useRouter } from 'next/router'
+import { PaymentMethodEntity } from '@/features/transaction-management/payment/payment.model'
 import { calculateRange } from '@/utils/calculateRange'
-import SearchingField from '@/core/components/fields/SearchingField'
 
 interface PaymentTableProps {
   isLoading: boolean
@@ -70,13 +64,24 @@ export const TransactionTableComponent = ({
   currentPage,
   onCurrentPageChange,
 }: PaymentTableProps) => {
-  const router = useRouter()
+  const { classes } = useStyles()
+  const tableRef = useRef<HTMLDivElement>(null) // Reference to the table
 
-  const { classes, cx } = useStyles()
-  const [selection, setSelection] = useState(['-1'])
+  // Existing state and functions...
+  const [customLoading, setCustomLoading] = useState(isLoading)
+  const [reactTableIndex, setReactTableIndex] = useState(currentPage)
   const [rowPerPage, setRowPerPage] = useState<string | null>('5')
   const [page, setPage] = useState(1)
   const [total, setTotal] = useState(100)
+
+  useEffect(() => {
+    setCustomLoading(isLoading)
+  }, [isLoading])
+
+  useEffect(() => {
+    setReactTableIndex(currentPage - 1)
+  }, [currentPage])
+
   const [startIndex, endIndex] = calculateRange(
     page,
     Number((rowPerPage && rowPerPage) ?? 0),
@@ -105,16 +110,6 @@ export const TransactionTableComponent = ({
         accessorKey: 'cashier_id',
         header: 'Id Cashier',
       },
-      // {
-      //   accessorKey: 'price',
-      //   header: 'Harga',
-      //   cell: ({ getValue }) => {
-      //     return (
-      //       <div>{`${formatNumberIndoCurrency(Number(getValue() || 0))}`}</div>
-      //     )
-      //   },
-      // },
-
       {
         accessorKey: 'created_at',
         header: 'Created At',
@@ -123,80 +118,35 @@ export const TransactionTableComponent = ({
           return <div>{format(new Date(date), 'dd/MM/yyyy')}</div>
         },
       },
-
       {
         id: 'actions',
         accessorKey: 'id',
         header: 'Actions',
-        cell: ({ getValue }) => {
+        cell: ({ getValue, row }) => {
           return (
-            <>
-              <Group spacing="xs" noWrap>
-                <Link
-                  href={`/transaction-managaement/transaction/detail-transaction${getValue()}`}
-                >
-                  <Button variant="outline" size="sm" color="blue">
-                    Show
-                  </Button>
-                </Link>
-                {/* <Link
-                  href={`/transaction-managaement/transaction/detail-transaction${getValue()}`}
-                > */}
-                <Button variant="outline" size="sm" color="red">
-                  Print Data
+            <Group spacing="xs" noWrap>
+              <Link
+                href={`/transaction-management/transaction/detail-transaction${getValue()}`}
+              >
+                <Button variant="outline" size="sm" color="blue">
+                  Show
                 </Button>
-                {/* </Link> */}
-
-                {/* <Link href={`/academic/master-data/course/${getValue()}/edit`}>
-                  <Button variant="outline" size="sm" color="yellow">
-                    Edit
-                  </Button>
-                </Link> */}
-
-                {/* <Button variant="outline" size="sm" color="red">
-                  Delete
-                </Button> */}
-                {/* <Grid.Col span={5} offset={1}>
-                  <Flex justify={'flex-end'}>
-                    <SearchingField size="sm" />
-                    <Pagination
-                      // total={total / Number(rowPerPage)}
-                      total={Math.ceil(total / Number(rowPerPage))}
-                      position="center"
-                      onChange={setPage}
-                      styles={() => ({
-                        control: {
-                          '&[data-active]': {
-                            backgroundColor: '#018B14',
-                          },
-                          '&[data-active]:not([data-disabled]):hover': {
-                            backgroundColor: '#018B14',
-                          },
-                        },
-                      })}
-                    />
-                  </Flex>
-                </Grid.Col> */}
-              </Group>
-            </>
+              </Link>
+              <Button
+                variant="outline"
+                size="sm"
+                color="red"
+                onClick={() => handleExportPDF(row.original)}
+              >
+                Print Data
+              </Button>
+            </Group>
           )
         },
       },
     ],
     []
   )
-
-  const [customLoading, setCustomLoading] = React.useState(isLoading)
-
-  useEffect(() => {
-    setCustomLoading(isLoading)
-  }, [isLoading])
-
-  const [reactTableIndex, setReactTableIndex] = React.useState(currentPage)
-
-  useEffect(() => {
-    setReactTableIndex(currentPage - 1)
-  }, [currentPage])
 
   const pagination = React.useMemo(
     () => ({
@@ -209,15 +159,55 @@ export const TransactionTableComponent = ({
   const reactTable = useReactTable<PaymentMethodEntity>({
     data: data,
     getCoreRowModel: getCoreRowModel(),
-    // onStateChange(updater: Updater<TableState>): void {},
-    renderFallbackValue: undefined,
-    state: {
-      pagination,
-    },
+    state: { pagination },
     columns: columns,
     manualPagination: true,
     debugTable: true,
   })
+
+  // Function to handle exporting the PDF
+  const handleExportPDF = async (transaction: PaymentMethodEntity) => {
+    const pdf = new jsPDF()
+
+    // Set up the PDF layout
+    pdf.setFontSize(16)
+    pdf.text('Transaction Receipt', 20, 20)
+    pdf.setFontSize(12)
+
+    // Add details to the PDF
+    const leftColumnX = 20 // X-coordinate for the labels
+    const rightColumnX = 100 // X-coordinate for the values
+
+    // Add details to the PDF
+    // pdf.text('ID Transaksi:', leftColumnX, 40)
+    // pdf.text(transaction.id, rightColumnX, 40, { align: 'right' })
+
+    pdf.text('Metode Transaksi :', leftColumnX, 50)
+    pdf.text(transaction.payment_method_name, rightColumnX, 50, {
+      align: 'right',
+    })
+
+    pdf.text('Total Transaksi :', leftColumnX, 60)
+    pdf.text(transaction.total_transactions.toString(), rightColumnX, 60, {
+      align: 'right',
+    })
+
+    pdf.text('Nama Pembeli :', leftColumnX, 70)
+    pdf.text(transaction.name_customer, rightColumnX, 70, { align: 'right' })
+
+    pdf.text('Tanggal :', leftColumnX, 90)
+    pdf.text(
+      format(new Date(transaction.created_at), 'dd/MM/yyyy'),
+      rightColumnX,
+      90,
+      { align: 'right' }
+    )
+    // pdf.text('ID Cashier:', leftColumnX, 80)
+    // pdf.text(transaction.cashier_id, rightColumnX, 80, { align: 'right' })
+
+    // Save the PDF
+    pdf.save(`transaction-${transaction.id}.pdf`)
+  }
 
   return (
     <div>
@@ -225,72 +215,55 @@ export const TransactionTableComponent = ({
       {customLoading ? (
         <Loader />
       ) : (
-        <div className={'overflow-x-auto'}>
+        <div ref={tableRef} className={'overflow-x-auto'}>
           <MantineTable className={'table-auto'}>
             <thead>
-              {reactTable?.getHeaderGroups().map((headerGroup) => (
+              {reactTable.getHeaderGroups().map((headerGroup) => (
                 <tr key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => {
-                    return (
-                      <th key={header.id} colSpan={header.colSpan}>
-                        {header.isPlaceholder ? null : (
-                          <div>
-                            {flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                          </div>
-                        )}
-                      </th>
-                    )
-                  })}
+                  {headerGroup.headers.map((header) => (
+                    <th key={header.id} colSpan={header.colSpan}>
+                      {header.isPlaceholder ? null : (
+                        <div>
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                        </div>
+                      )}
+                    </th>
+                  ))}
                 </tr>
               ))}
             </thead>
             <tbody>
-              {reactTable?.getRowModel().rows.map((row) => {
-                return (
-                  <tr key={row.id}>
-                    {row.getVisibleCells().map((cell) => {
-                      return (
-                        <td key={cell.id}>
-                          {flexRender(
-                            cell.column.columnDef.cell,
-                            cell.getContext()
-                          )}
-                        </td>
-                      )
-                    })}
-                  </tr>
-                )
-              })}
+              {reactTable.getRowModel().rows.map((row) => (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))}
             </tbody>
           </MantineTable>
           <Pagination
             total={pageCount}
             onChange={(page) => {
               const index = page - 1
-              // console.log('page', page)
               reactTable.setPageIndex(index)
-
               onCurrentPageChange(page)
             }}
-            onPreviousPage={() => {
-              reactTable.previousPage()
-            }}
-            onNextPage={() => {
-              reactTable.nextPage()
-            }}
-            onFirstPage={() => {
-              reactTable.setPageIndex(0)
-            }}
-            onLastPage={() => {
+            onPreviousPage={() => reactTable.previousPage()}
+            onNextPage={() => reactTable.nextPage()}
+            onFirstPage={() => reactTable.setPageIndex(0)}
+            onLastPage={() =>
               reactTable.setPageIndex(reactTable.getPageCount() - 1)
-            }}
+            }
             value={reactTable.getState().pagination.pageIndex + 1}
-            // page={currentPage}
-
-            // onChange={setCurrent}
           />
         </div>
       )}
